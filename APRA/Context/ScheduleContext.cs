123d46f5acc911157f83f6256a4e6ec6377ddbb0
beck410 <beck410@hotmail.com>
2015-03-12 22:54:08 -0500

    added dbcontext for schedule

---
+++ APRA/Context/ScheduleContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using APRA.Models;
+
+namespace APRA.Context {
+  public class ScheduleContext : DbContext{
+
+    public ScheduleContext() : base("ScheduleContext") { }
+
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Caretaker> Caretakers { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Monitor> Monitors { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+
+  }
+}
