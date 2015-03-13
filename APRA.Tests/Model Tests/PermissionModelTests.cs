cfc8d3f39ce20b585301d0d65ebb51e2ad289c48
beck410 <beck410@hotmail.com>
2015-03-13 10:33:55 -0500

    permission model done with passing tests

--- APRA.Tests/Model Tests/PermissionModelTests.cs
+++ APRA.Tests/Model Tests/PermissionModelTests.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APRA.Models;
 
 namespace APRA.Tests.Model_Tests {
   [TestClass]
   public class PermissionModelTests {
     [TestMethod]
-    public void AddMonitorSuccess() {
+    public void AddPermissionAdminSuccess() {
+      Permission user1 = new Permission(3,"admin");
+    }
+
+    [TestMethod]
+    public void AddPermissionPatientSuccess() {
+      Permission user1 = new Permission(3,"patient");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void AddPermissonInvalidRole() {
+      Permission user2 = new Permission(3,"user");
     }
   }
 }
--- APRA/Models/Permission.cs
+++ APRA/Models/Permission.cs
@@ -8,7 +8,23 @@ namespace APRA.Models {
 
     public int PermissionId { get; set; }
     public int PatientId { get; set; }
+    //admin or patient
     public string Role { get; set; }
 
+    public Permission(int patientId, string role) {
+      this.PatientId = patientId;
+      _checkRoleValidity(role);
+    }
+
+    private void _checkRoleValidity(string role) {
+      string[] roles = { "admin","patient" };
+
+      if (roles.Contains(role) == true) {
+        this.Role = role;
+        return;
+      }
+
+      throw new ArgumentException("role is not valid");
+    }
   }
 }
