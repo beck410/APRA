f9b680512cbe7f2f34f31b7c8377a5acc82bc298
beck410 <beck410@hotmail.com>
2015-03-12 22:53:40 -0500

    added tests for patient model - passing

---
+++ APRA.Tests/Model Tests/PatientModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APRA.Models;
+
+namespace APRA.Tests.Model_Tests {
+  [TestClass]
+  public class PatientModel {
+
+    [TestMethod]
+    public void AddPatientSuccess() {
+      Patient john = new Patient("John Smith","Nashville, TN", 12);
+      Assert.AreEqual("John Smith", john.Name);
+      Assert.AreEqual("Nashville, TN", john.Location);
+      Assert.AreEqual(12, john.UserId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void AddInvalidPatientWithEmptyName() {
+      Patient john = new Patient("", "Nashville, TN", 12);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void AddInvalidPatientWithWhiteSpace() {
+      Patient john = new Patient("  ", "Nashville, TN", 12);
+    }
+  }
+}
