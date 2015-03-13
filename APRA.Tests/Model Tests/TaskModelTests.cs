3cc9886a9ea4b89988c8117eba049672574e03f8
beck410 <beck410@hotmail.com>
2015-03-13 13:06:19 -0500

    added task model with passing tests

---
+++ APRA.Tests/Model Tests/TaskModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APRA.Models;
+
+namespace APRA.Tests.Model_Tests {
+  [TestClass]
+  public class TaskModelTests {
+    [TestMethod]
+    public void AddTaskSuccess() {
+      Task breakfast = new Task(3,5,"breakfast","cereal and a banana - take pills with food","morning","http://www.aws.com/photolink",new DateTime(2015,5,4));
+      Assert.AreEqual(3, breakfast.PatientID);
+      Assert.AreEqual(5, breakfast.Week);
+      Assert.AreEqual("breakfast", breakfast.TaskName);
+      Assert.AreEqual("cereal and a banana - take pills with food" ,breakfast.TaskNotes);
+      Assert.AreEqual("http://www.aws.com/photolink", breakfast.ImageLink);
+      Assert.AreEqual(new DateTime(2015, 5, 4), breakfast.Date);
+      Assert.AreEqual(null, breakfast.Difficulty);
+      Assert.AreEqual(null, breakfast.Stress);
+      Assert.AreEqual(null, breakfast.StartTime);
+      Assert.AreEqual(null, breakfast.EndTime);
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void AddInvalidTaskEmptyTaskName() {
+      Task breakfast = new Task(3,5," ","cereal and a banana - take pills with food","morning","http://www.aws.com/photolink",new DateTime(2015,5,4));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void AddInvalidTaskInvalidWeek() {
+      Task breakfast = new Task(3,57," ","cereal and a banana - take pills with food","morning","http://www.aws.com/photolink",new DateTime(2015,5,4));
+    }
+  }
+}
--- APRA/Models/Task.cs
+++ APRA/Models/Task.cs
@@ -2,23 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace APRA.Models {
   public class Task {
 
-    public int taskId { get; set; }
-    public int patientID { get; set; }
-    public int week { get; set; }
-    public string taskName { get; set; }
-    public string timeOfDay { get; set; }
-    public string taskNotes { get; set; }
-    public int imageLink { get; set; }
-    public DateTime date { get; set; }
-    public int difficulty { get; set; }
-    public int stress { get; set; }
-    public string feedbackNotes { get; set; }
+    [Required]
+    public int TaskId { get; set; }
+    [Required]
+    public int PatientID { get; set; }
+    //between 1-52
+    [Required]
+    [Range(1,52)]
+    public int Week { get; set; }
+    [Required]
+    public string TaskName { get; set; }
+    //morning,lunch,afternoon,night
+    [Required]
+    public string TimeOfDay { get; set; }
+    public string TaskNotes { get; set; }
+    //must be http link
+    public string ImageLink { get; set; }
+    [Required]
+    public DateTime Date { get; set; }
+    //1-10 or null
+    [Range(1,10)]
+    public int? Difficulty { get; set; }
+    //1-10 or null
+    [Range(1,10)]
+    public int? Stress { get; set; }
+    public string FeedbackNotes { get; set; }
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
 
+    public Patient Patient { get; set; }
 
-    public Patient patient { get; set; }
+    public Task(int patientId, int week, string taskName, string taskNotes, string timeOfDay, string imageLink, DateTime date) {
+
+      _isEmpty(taskName, week.ToString());
+
+      this.PatientID = patientId;
+      this.Week = week;
+      this.TaskName = taskName;
+      this.TaskNotes = taskNotes;
+      this.ImageLink = imageLink;
+      this.Date = date;
+    }
+
+    private void _isEmpty(string name, string relationship) {
+      if (String.IsNullOrWhiteSpace(name) || String.IsNullOrWhiteSpace(relationship))
+        throw new ArgumentException("task name and week must have a value");
+    }
   }
 }
